5b632f2d7f9e36dc31dac6c6e46e8ab621d1900f
NichWes <nicholaswestley06@gmail.com>
2025-10-08 22:32:53 +0700

    membuat Bagian Main

--- src/com/uts/MainApp.java
+++ src/com/uts/MainApp.java
@@ -1,7 +1,28 @@
 package com.uts;
 
-public class MainApp {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main.fxml"));
+            Scene scene = new Scene(loader.load());
+            scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
+            primaryStage.setTitle("Sistem Manajemen Perpustakaan Mini");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/com/uts/controllers/MainController.java
+++ src/com/uts/controllers/MainController.java
@@ -1,5 +1,34 @@
 package com.uts.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class MainController {
-    
+
+    public void openBooks(ActionEvent event) {
+        openWindow("/view/books.fxml", "Manajemen Buku");
+    }
+
+    public void openMembers(ActionEvent event) {
+        openWindow("/view/members.fxml", "Manajemen Anggota");
+    }
+
+    public void openTransactions(ActionEvent event) {
+        openWindow("/view/transactions.fxml", "Peminjaman Buku");
+    }
+
+    private void openWindow(String resource, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
+            Scene scene = new Scene(loader.load());
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
